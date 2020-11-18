ba980c4c7efc5e8b0d6c72b45e01ed96996af24c
Tatu <tatu@castren.com>
2020-11-18 16:03:57 +0200

    Viikko4. tehtävät 1-3

---
+++ viikko4/Verkkokauppa/src/test/java/ohtu/KauppaTest.java
@@ -0,0 +1,34 @@
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    Kauppa k;
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+        varasto = mock(Varasto.class);
+        k = new Kauppa(varasto, pankki, viite);    
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5)); 
+    }
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+    k.aloitaAsiointi();
+    k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+    k.tilimaksu("pekka", "12345");
+
+    // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+    verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
+    // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+}
