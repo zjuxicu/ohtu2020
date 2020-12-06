6d3086a3845b566338a4dc9ff7402a0c61bac762
Tatu <tatu@castren.com>
2020-12-06 11:33:21 +0200

    tehtävät 1-2

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/All.java
@@ -0,0 +1,12 @@
+package statistics.matcher;
+
+import statistics.Player;
+
+public class All implements Matcher {
+
+    @Override
+    public boolean matches(Player p) {
+        return true;
+    }
+    
+}
